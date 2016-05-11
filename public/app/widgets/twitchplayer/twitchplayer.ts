69e16484e32fd57afc102e35993bd7b626968197
Dan <dan.geffroy@capgemini.com>
2016-05-11 14:39:50 +0200

    Twitch player can now be changed from it's default value (adding monstercat as default)

--- public/app/widgets/twitchplayer/twitchplayer.ts
+++ public/app/widgets/twitchplayer/twitchplayer.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import { OnInit } from '@angular/core';
 
 declare var Twitch:any;
+declare var jQuery:any;
 
 @Component({
   selector: 'twitchplayer',
@@ -9,18 +10,26 @@ declare var Twitch:any;
   styleUrls: ['app/widgets/twitchplayer/twitchplayer.css']
 })
 export class Twitchplayer implements OnInit{
-
   player;
+  channel : String;
 
   ngOnInit(){
-    console.log("executed?")
        var options = {
-           channel: "tarik_tv",
+           channel: "monstercat",
            width: "100%",
            height: "100%",
        };
        this.player = new Twitch.Player("twitchPlayer", options);
        this.player.setVolume(0.5);
-       this.player.play();
+  }
+  newUrl($event){
+    jQuery("#twitchPlayer").empty();
+    var options = {
+        channel: this.channel,
+        width: "100%",
+        height: "100%",
+    };
+    this.player = new Twitch.Player("twitchPlayer", options);
+    this.player.setVolume(0.5);
   }
 }
