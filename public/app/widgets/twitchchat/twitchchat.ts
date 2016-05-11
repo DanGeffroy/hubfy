2bb0bd00a905911e3741b309830177790da1e37f
Dan <dan.geffroy@capgemini.com>
2016-05-11 16:29:02 +0200

    Adding twitch chat search field

--- public/app/widgets/twitchchat/twitchchat.ts
+++ public/app/widgets/twitchchat/twitchchat.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-
+import {Component, Input, ViewChild, Renderer} from '@angular/core';
+declare var jQuery:any;
 
 @Component({
   selector: 'twitchchat',
@@ -7,4 +7,13 @@ import {Component} from '@angular/core';
   styleUrls: ['app/widgets/twitchchat/twitchchat.css']
 })
 export class Twitchchat {
+  @Input() channel : String = "monstercat";
+
+  src : String = "http://www.twitch.tv/"+this.channel+"/chat";
+
+  constructor(public renderer: Renderer) {}
+  newUrl($event){
+    var newUrl = "http://www.twitch.tv/"+this.channel+"/chat";
+      jQuery("#chat_embed").attr("src", newUrl);
+  }
 }
