15a10443136a83a8284559fd6bfefadff958dd9a
Dan <dan.geffroy@capgemini.com>
2016-05-11 11:03:26 +0200

    Adding interactive version of tiwtch chat with ngOnInit directive

--- public/app/widgets/twitchplayer/twitchplayer.ts
+++ public/app/widgets/twitchplayer/twitchplayer.ts
@@ -1,10 +1,26 @@
 import {Component} from '@angular/core';
+import { OnInit } from '@angular/core';
 
+declare var Twitch:any;
 
 @Component({
   selector: 'twitchplayer',
   templateUrl: 'app/widgets/twitchplayer/twitchplayer.html',
   styleUrls: ['app/widgets/twitchplayer/twitchplayer.css']
 })
-export class Twitchplayer {
+export class Twitchplayer implements OnInit{
+
+  player;
+
+  ngOnInit(){
+    console.log("executed?")
+       var options = {
+           channel: "tarik_tv",
+           width: "100%",
+           height: "100%",
+       };
+       this.player = new Twitch.Player("twitchPlayer", options);
+       this.player.setVolume(0.5);
+       this.player.play();
+  }
 }
