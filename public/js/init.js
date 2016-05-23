ddc4262a3b39194817dfb6bac465175de0db0582
Dan <dan.geffroy@capgemini.com>
2016-05-23 14:53:52 +0200

    Adding password check on register

--- public/js/init.js
+++ public/js/init.js
@@ -6,29 +6,15 @@
 
         // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
         $('.modal-trigger').leanModal();
-        bindLinks("#youtubeAddButton", callBackYoutube);
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-function bindLinks(href, callBack) {
-    $(href).click(function (event) {
-        event.preventDefault();
-
-        var url = $(this).attr('href');
-        $.ajax({
-            type: 'GET'
-            , url: url
-            , contentType: 'application/json'
-            , success: callBack
-        });
+$(function(){
+  $("#password_check").keyup(function() {
+      if($(this).val()!=$(".password").val()){
+        $(this).get(0).setCustomValidity("Passwords Don't Match");
+      }else{
+        $(this).get(0).setCustomValidity("");
+      }
     });
-};
-
-function callBackYoutube(data) {
-    if(data.status === "error"){
-         Materialize.toast('Youtube player already on hub', 3000, 'rounded');
-    }else{
-        //refreshing
-        location.reload();
-    }
-}
+  });
